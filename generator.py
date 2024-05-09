3beef7149ab8dbff0aa5c94fa9c8fca96e898749
William957-web <85293841+William957-web@users.noreply.github.com>
2024-05-09 22:28:46 +0800

    Update generator.py

--- generator.py
+++ generator.py
@@ -6,7 +6,7 @@ import os
 #payload = Payload('echo "pwned by whale120" > proof.txt')
 db = SqliteDict("product.sqlite")
 db["1"] = {"name":"Edible ramen", "description":"It's a delicious and quite well knowned dish.", "price":"10"}
-db["2"] = {"name":"Edible corn", "description":"Yummy OuO", "price":"99999999999"}
-db["3"] = {"name":"Edible whale", "description":"womp oWo?", "price":"99999999999"}
+db["2"] = {"name":"Edible corn", "description":"Yummy OuO", "price":"999999999999"}
+db["3"] = {"name":"Edible whale", "description":"womp oWo?", "price":"999999999999"}
 db.commit()
 db.close()
